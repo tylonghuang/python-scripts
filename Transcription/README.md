4685449e56336cb87b220b426d5739205503306b
Long Hoang Nguyen <lungnguyenhung@gmail.com>
2021-01-14 19:08:25 +0100

    Added Transcription and dependencies

---
+++ Transcription/README.md
@@ -0,0 +1,30 @@
+# Video Transcription with Python
+
+This repository contains a simple Python command line script that transcribes text from short video files with MoviePy and SpeechRecognition.
+
+> **NOTE**: The script uses the [Google Web Speech API](https://wicg.github.io/speech-api) because of the present default API key which is both convenient and free. Sadly it only supports smaller video files as of now. For bigger files or production use you might want to have a look at [Google Cloud Speech](https://cloud.google.com/speech-to-text). It has a payment model and requires registration which would defeat the purpose of this script.
+
+## Requirements
+
+- [Python](https://www.python.org/downloads)
+- [pip](https://pip.pypa.io/en/stable/)
+- [MoviePy](https://pypi.org/project/moviepy) to convert the video files to audio data
+- [SpeechRegognition](https://pypi.org/project/SpeechRecognition) which functions as a wrapper for multiple recognition APIs
+
+> **NOTE**: The requirements for this script were already included in the pipfile.
+
+## Usage:
+
+Command line arguments for the script:
+
+```
+$ python transcription.py -h/--help
+$ python transcription.py -v/--video-file <file_name>
+```
+
+> **NOTE**: The video has to be in the same directory as the script.
+
+Have fun!
+___
+
+_For any inquiries or questions contact me under: lungnguyenhung@gmail.com_
---
+++ Transcription/transcription.py
@@ -0,0 +1,53 @@
+# Necessary imports
+import moviepy.editor as mp
+import speech_recognition as sr
+
+import argparse
+
+# Define command line argument(s)
+parser = argparse.ArgumentParser(description="Transcribe text from a video file")
+parser.add_argument("-v", "--video-file", type=str, metavar="", required=True,
+ help="name of the video file e.g. video.mp4")
+args = parser.parse_args()
+
+# Convert video file to textual data
+def video_to_text(file_name):
+    video_input = mp.VideoFileClip(r""+file_name)
+    stripped_filename = file_name.split(".", maxsplit=1)[0] + ".wav"
+    audio_output = video_input.audio.write_audiofile(r""+stripped_filename)
+    r = sr.Recognizer()
+    audio_file = sr.AudioFile(stripped_filename)
+    with audio_file as source:
+        # Uncomment if there is a lot of background noise:
+        # r.adjust_for_ambient_noise(source)
+        # You might change the starting point and duration of the audio data:
+        # audio_data = r.record(source, offset=x, duration=y)
+        audio_data = r.record(source)
+    try:
+        # Make changes HERE if you want to use a different API (i.e. Google Cloud Speech)
+        # Language codes: https://cloud.google.com/speech-to-text/docs/languages
+        # Default value is English
+        text_data = r.recognize_google(audio_data, language="en-US")
+        return text_data
+    except sr.UnknownValueError:
+        print("Did not understand the audio. Please try again")
+    except sr.RequestError as e:
+        print("Requesting results from Google Speech Recognition is not possible atm; {0}"
+        .format(e))
+          
+# Write textual data to a txt file
+def write_to_file(text):
+    with open("transcription.txt", mode="w") as f:
+        f.write("Text from video:")
+        f.write("\n")
+        f.write(text)
+        f.close()
+    return f
+
+def main():
+    text = video_to_text(args.video_file)
+    write_to_file(text)
+    print("Successful transcription!")
+    
+if __name__ == "__main__":
+    main()
